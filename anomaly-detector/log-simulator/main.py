8c973fcb84c126f4424eeffa8fb47375ed49621d
Ankan Das <dasankan84@gmail.com>
2025-05-28 21:22:01 +0530

    Project Restructure: Anomaly Detection

---
+++ anomaly-detector/log-simulator/main.py
@@ -0,0 +1,17 @@
+import json, random, time
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+
+while True:
+    log = {
+        "timestamp": time.time(),
+        "service": random.choice(["auth", "payment", "orders"]),
+        "latency_ms": random.gauss(200, 50),
+        "error_rate": random.random()
+    }
+    producer.send('logs', log)
+    print("Sent:", log)
+    time.sleep(1)
