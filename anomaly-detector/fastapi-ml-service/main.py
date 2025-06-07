0e591db02d72adab78220eb79f6019462621fdfc
Ankan Das <dasankan84@gmail.com>
2025-06-07 22:45:41 +0530

    Added Prometheus and Grafana

--- anomaly-detector/fastapi-ml-service/main.py
+++ anomaly-detector/fastapi-ml-service/main.py
@@ -2,12 +2,26 @@ from fastapi import FastAPI, Request
 import joblib
 import numpy as np
 
+from prometheus_client import start_http_server, Counter
+
+REQUESTS = Counter("Total", "Total /predict requests")
+ANOMALIES = Counter("Anomalies", "Anomalies predicted")
+
+# Start Prometheus metrics server
+start_http_server(8001)  # 👈 Starts a separate server at :8000/metrics
+
 app = FastAPI()
 model = joblib.load("model.joblib")     # Pre-trained Isolation Forest
 
 @app.post("/predict")
 async def predict(request: Request):
+    REQUESTS.inc()
+    
     payload = await request.json()
     features = np.array([[payload['latency_ms'], payload['error_rate']]])
     score = model.predict(features)[0]
+    
+    if score == -1:
+        ANOMALIES.inc()
+    
     return {"anomaly": bool(score == -1)}
--- anomaly-detector/processor/main.py
+++ anomaly-detector/processor/main.py
@@ -2,6 +2,13 @@ import json
 import requests
 from kafka import KafkaConsumer
 
+from prometheus_client import start_http_server, Counter
+
+MESSAGES = Counter("processor_messages", "Messages received from Kafka")
+ANOMALIES = Counter("processor_anomalies", "Anomalies detected")
+
+start_http_server(9000)
+
 consumer = KafkaConsumer('logs', bootstrap_servers='kafka:9092',
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
